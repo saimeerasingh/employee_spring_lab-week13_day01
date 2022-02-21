192b1fb31f077d737e3e3df3fbef26f1a1b249e8
saimeerasingh <singhsaimeera@gmail.com>
2022-02-21 14:17:48 +0000

    file setup with spring initializer

---
+++ src/test/java/com/codeclan/example/employeeTracking/EmployeeTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeeTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
